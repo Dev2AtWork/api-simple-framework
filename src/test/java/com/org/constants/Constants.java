0f7fb4a78f4c69e967ea5f53b4ccb1bdc0cc1c21
Abhi <nandan.abhi10@gmail.com>
2018-07-23 15:47:13 +0000

    First commit

---
+++ src/test/java/com/org/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.org.constants;
+
+public class Constants {
+	public static String BASE_URL = "https://api.darksky.net/forecast/7a3f192f51ce44090d7309c03385e2ae";
+	public static String query = "37.8267,-122.4233";
+	
+}
