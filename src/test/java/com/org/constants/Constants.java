1de2419cb41a0497e3edda069bbb11df69debdea
Abhi <nandan.abhi10@gmail.com>
2018-07-24 09:13:32 +0000

    tests updated with new scenarios

--- src/test/java/com/org/constants/Constants.java
+++ src/test/java/com/org/constants/Constants.java
@@ -1,7 +1,8 @@
 package com.org.constants;
 
 public class Constants {
-	public static String BASE_URL = "https://api.darksky.net/forecast/7a3f192f51ce44090d7309c03385e2ae";
-	public static String query = "37.8267,-122.4233";
+	public static String BASE_URL = "https://api.darksky.net/forecast";
+	public static String AUTH_KEY = "7a3f192f51ce44090d7309c03385e2ae";
+	public static String COORDINATES = "37.8267,-122.4233";
 	
 }
---
+++ src/test/java/com/org/tests/DarkSkyAPITest.java
@@ -0,0 +1,89 @@
+package com.org.tests;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.org.constants.Constants;
+import com.org.dataprovider.JsonArrTestDataProvider;
+import com.org.dataprovider.SchemaDataProvider;
+
+public class DarkSkyAPITest {
+
+	Response response;
+
+	/*
+	 * This is Before Test that calls the API
+	 */
+	@BeforeClass
+	public void callAPI() {
+		System.out.println("Executing Before...");
+		response = given().baseUri(Constants.BASE_URL + "/" + Constants.AUTH_KEY).log().everything()
+				.contentType(ContentType.JSON).when().get("/" + Constants.COORDINATES).then()
+				.contentType(ContentType.JSON).statusCode(200).extract().response();
+	}
+
+	/*
+	 * This test validates top level schema
+	 */
+	@Test
+	public void validateResponseTopLevelStructure() {
+		try {
+			HashMap<String, Object> res = response.body().jsonPath().get("$");
+			HashMap<String, String> schema = SchemaDataProvider.getTestData();
+			for (String key : schema.keySet()) {
+				assertTrue(schema.get(key).equalsIgnoreCase(getDataType(res.get(key))));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/*
+	 * This test validates Array Size of daily, hourly & minutely objects
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void validateJSONArrayCount() {
+		try {
+			HashMap<String, String> testData = JsonArrTestDataProvider.getTestData();
+			for (String key : testData.keySet()) {
+				assertTrue("Array Size Validation for " + key,
+						((ArrayList<Object>) response.body().jsonPath().get(key + ".data")).size() == Integer
+								.parseInt(testData.get(key)));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * This test validates coordinates in response is same as the same in request
+	 */
+	@Test
+	public void validateCoordinatesInResponse() {
+		assertTrue("Validation of coordinates in response",
+				(response.body().jsonPath().get("latitude") + "," + response.body().jsonPath().get("longitude"))
+						.equalsIgnoreCase(Constants.COORDINATES));
+	}
+
+	public String getDataType(Object obj) {
+		if (obj instanceof Integer)
+			return "int";
+		if (obj instanceof Float)
+			return "float";
+		if (obj instanceof String)
+			return "string";
+		if (obj instanceof Boolean)
+			return "boolean";
+		return "object";
+	}
+}
